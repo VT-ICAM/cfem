5763f856d9feeae860e56edada121250ac7fea9e
David Wells <drwells@vt.edu>
2014-06-05 15:08:10 -0400

    Moved the diagonal multiplier into the misc category.

--- misc.c
+++ misc.c
@@ -1,4 +1,25 @@
 /** @file */
+/**
+ * @brief Multiply a matrix by a diagonal matrix (represented as a flat array)
+ * in place on the right.
+ *
+ * @param[in] rows Number of rows in the matrix.
+ * @param[in] cols Number of columns in the matrix.
+ * @param[out] matrix The matrix. It will be multiplied in-place on the right by
+ * the diagonal matrix.
+ * @param[in] diagonal The diagonal matrix, represented as a 1D array.
+ */
+void cf_diagonal_multiply(const int rows, const int cols,
+                          double matrix[static rows*cols],
+                          double diagonal[static cols]) {
+        int i, j;
+        /* traverse the matrix in row-major order. */
+        for (i = 0; i < rows; i++) {
+                for (j = 0; j < cols; j++) {
+                        matrix[CF_INDEX(i, j, rows, cols)] *= diagonal[j];
+                }
+        }
+}
 
 /**
  * @brief Take the integer power of an integer.
